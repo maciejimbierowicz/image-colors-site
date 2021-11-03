f4518c66f62fbbccd4190f757b98882f60dd9e45
Imbir <imbierowicz.maciej@gmail.com>
2021-11-03 17:04:55 +0100

    modified wsgi

--- main.py
+++ main.py
@@ -60,5 +60,5 @@ def colour_pallete(image):
     return palette_hex
 
 
-if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=80, debug=True)
+# if __name__ == '__main__':
+#       app.run(host='0.0.0.0', port=80, debug=True)
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from main import app as application
+
+app = application
+
+if __name__ == '__main__':
+      app.run()
